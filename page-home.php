74392022bc075ead9a8fa135bee529e95622dc5c
JorgeQZ <jorge_qzg@hotmail.com>
2021-01-21 12:19:13 -0600

    custom files

---
+++ page-home.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template Name: Home
+ */
+
+
+ get_header();
+?>
+
+<div class="container-news">
+    <!-- Column -->
+    <div class="column">
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <h4>1</h4>
+            </div>
+            <div class="item">
+                <h4>2</h4>
+            </div>
+            <div class="item">
+                <h4>3</h4>
+            </div>
+            <div class="item">
+                <h4>4</h4>
+            </div>
+            <div class="item">
+                <h4>5</h4>
+            </div>
+            <div class="item">
+                <h4>6</h4>
+            </div>
+            <div class="item">
+                <h4>7</h4>
+            </div>
+            <div class="item">
+                <h4>8</h4>
+            </div>
+            <div class="item">
+                <h4>9</h4>
+            </div>
+            <div class="item">
+                <h4>10</h4>
+            </div>
+            <div class="item">
+                <h4>11</h4>
+            </div>
+            <div class="item">
+                <h4>12</h4>
+            </div>
+        </div>
+    </div><!-- Column -->
+
+    <!-- Column -->
+    <div class="column">
+        <div class="item"></div>
+        <div class="item"></div>
+    </div><!-- Column -->
+</div>
+
+<?php get_footer();?>
