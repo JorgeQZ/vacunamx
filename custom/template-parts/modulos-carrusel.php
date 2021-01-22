963415728f3f58bd0272467ab382664e9aa8324e
Byron-Guevara <byronguevara@akevia.mx>
2021-01-22 11:39:59 -0600

    generals class

---
+++ custom/template-parts/modulos-carrusel.php
@@ -0,0 +1,21 @@
+
+<div class="contenedor-general-modulos">
+
+    <div class="contenedor-wrapper">
+        <div class="contenedor-item">
+            <div class="cont-pregunta">
+                <h2>¿Es seguro vacunarse?</h2>
+                <p>
+                    <span> La vacuna es segura. </span> Todas las vacunas aprobadas son sometidas a <span> pruebas rigurosas </span> a lo largo de las diferentes 
+                    fases de los ensayos clínicos, y siguen siendo evaluadas regularmente una vez comercializadas. Los científicos 
+                    también siguen constantemente la información procedente de diferentes fuentes en busca de indicios de que una 
+                    vacuna pueda tener efectos adversos.
+                </p>
+            </div>
+            <div class="cont-imagen">
+                <img src="<?php echo get_template_directory_uri().'/custom/img/es-seguro-vacunarse.png';?>" alt="">
+            </div>
+        </div>
+    </div>
+
+</div>
--- page-home.php
+++ page-home.php
@@ -7,6 +7,8 @@
  get_header();
 ?>
 
+<?php get_template_part( 'custom/template-parts/modulos-carrusel' ); ?>
+
 <?php get_template_part( 'custom/template-parts/grid-news' ); ?>
 
 <?php get_footer();?>
