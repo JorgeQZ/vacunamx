509ee8c7ed213ffd2fdba7c46ea91f021a44c38e
JorgeQZ <jorge_qzg@hotmail.com>
2021-01-21 18:11:27 -0600

    news grid  mobile and desktop

---
+++ custom/template-parts/grid-news.php
@@ -0,0 +1,102 @@
+<div class="container-news">
+    <!-- Column -->
+
+    <?php
+    $carousel_post = get_posts(
+        array(
+            'posts_per_page' => 6,
+            'category' => 5,
+            'post_tatus' => 'published',
+            'orderby'          => 'publish_date',
+            'order'            => 'DESC',
+        )
+    );
+    ?>
+    <div class="column">
+        <div class="owl-carousel owl-theme grid-news-carousel">
+            <?php
+            if ( $carousel_post ):
+                foreach ( $carousel_post as $post ) :
+                    setup_postdata( $post );
+                    ?>
+            <div class="item">
+
+                <div class="overlay" style="background-image: url( <?php echo get_the_post_thumbnail_url('','large') ?>) !important; background-size: cover !important; background-position: center !important;"></div>
+
+                <div class="content">
+                    <div class="title">
+                        <h3><?php the_title(); ?></h3>
+                    </div>
+                    <div class="excerpt">
+                        <p><?php the_excerpt(); ?></p>
+                    </div>
+                </div>
+
+                <div class="seemore">
+                    <a href="<?php the_permalink() ?>" title="Ver más: Ir al enlace">
+                        <span>Ver más</span>
+                        <span><img src="<?php echo get_template_directory_uri().'/custom/img/right-arrow-white.png'?>" alt="Ir al enlace"></span>
+                    </a>
+                </div>
+            </div>
+            <?php
+                endforeach;
+                wp_reset_postdata();
+            endif;
+            wp_reset_postdata();
+            ?>
+        </div>
+    </div><!-- Column -->
+
+    <!-- Column -->
+    <div class="column">
+        <?php
+         $latestpost = get_posts(
+            array(
+                'posts_per_page' => 2,
+                'post_tatus' => 'published',
+                'orderby'          => 'publish_date',
+                'order'            => 'DESC',
+            )
+        );
+
+        if ( $latestpost ):
+            foreach ( $latestpost as $post ) :
+                setup_postdata( $post );
+                ?>
+
+        <div class="row-item">
+
+
+            <div class="overlay" style="background-image: url( <?php echo get_the_post_thumbnail_url('','large') ?>) !important; background-size: cover !important;  background-position: center !important;"></div>
+
+            <div class="content">
+                <div class="title">
+                    <h3><?php the_title(); ?></h3>
+                </div>
+            </div>
+
+            <div class="seemore">
+                <a href="<?php the_permalink() ?>" title="Ver más: Ir al enlace">
+                    <span><img src="<?php echo get_template_directory_uri().'/custom/img/right-arrow-white.png'?>" alt="Ir al enlace"></span>
+                </a>
+            </div>
+        </div>
+        <?php
+            endforeach;
+            wp_reset_postdata();
+        endif;
+        wp_reset_postdata();
+        ?>
+
+    </div><!-- Column -->
+</div>
+
+<script>
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    items: 1
+})
+</script>
--- page-home.php
+++ page-home.php
@@ -7,65 +7,6 @@
  get_header();
 ?>
 
-<div class="container-news">
-    <!-- Column -->
+<?php get_template_part( 'custom/template-parts/grid-news' ); ?>
 
-    <?php
-    $lastposts = get_posts(
-        array(
-            'posts_per_page' => 6
-        )
-    );
-
-
-    ?>
-    <div class="column">
-        <div class="owl-carousel owl-theme grid-news-carousel">
-            <?php
-            if ( $lastposts ):
-                foreach ( $lastposts as $post ) :
-                    setup_postdata( $post ); ?>
-            <div class="item">
-
-                <div class="overlay" style="background-image: url( <?php echo get_the_post_thumbnail_url('','large') ?>)"></div>
-
-                <div class="content">
-                    <div class="title">
-                        <h3><?php the_title(); ?></h3>
-                    </div>
-                    <div class="excerpt">
-                        <p><?php the_excerpt(); ?></p>
-                    </div>
-                </div>
-
-                <div class="seemore">
-                    <a href="<?php the_permalink() ?>">
-                        <span>Ver más</span>
-                    </a>
-                </div>
-            </div>
-            <?php
-                endforeach;
-                wp_reset_postdata();
-            endif;
-            wp_reset_postdata();
-            ?>
-        </div>
-    </div><!-- Column -->
-
-    <!-- Column -->
-    <div class="column">
-        <div class="item"></div>
-        <div class="item"></div>
-    </div><!-- Column -->
-</div>
-
-<script>
-$('.owl-carousel').owlCarousel({
-    loop: false,
-    margin: 0,
-    nav: false,
-    items: 1
-})
-</script>
 <?php get_footer();?>
