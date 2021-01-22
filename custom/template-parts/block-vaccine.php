69e95080b4f73f781984c0a9e0253051ebf189e5
JorgeQZ <jorge_qzg@hotmail.com>
2021-01-22 11:52:44 -0600

    remove default margins & paddings

--- custom/template-parts/block-vaccine.php
+++ custom/template-parts/block-vaccine.php
@@ -1,7 +1,7 @@
 <div class="container-vaccine">
     <div class="contenedor-wrapper vaccine-content">
         <div class="info-box">
-            Total de personas vacunadas al día de hoy
+            Total de personas vacunadas <strong>al día de hoy</strong>
         </div>
         <div class="img-vaccine">
             <img src="<?php echo get_template_directory_uri().'/custom/img/vaccine.png'?>" alt="Vacuna">
