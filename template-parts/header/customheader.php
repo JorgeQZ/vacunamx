c57e9d8451bd9b04dc956c075dfbb47bc66a59a1
Byron-Guevara <byronguevara@akevia.mx>
2021-02-05 15:40:47 -0600

    menu y detalles

--- template-parts/header/customheader.php
+++ template-parts/header/customheader.php
@@ -6,19 +6,15 @@
             <img src="<?php echo get_template_directory_uri().'/custom/img/logo-gobiernodemexico.png';?>" alt="">
         </div>
 
-        <div class="contenedor-menu">
-            <ul class="menu-wrapper">
-                <li class="active"> <a href="#"> Home </a> </li>
-                <li> <a href="#"> Información de la vacuna </a> </li>
-                <li style="max-width: 130px"> <a href="#" style="width: 106px;"> Priorización de personas a vacunar </a> </li>
-                <li> <a href="#"> Calendario vacunación </a> </li>
-                <li> <a href="#"> Eventos adversos </a> </li>
-                <li> <a href="#"> Datos </a> </li>
-                <li> <a href="#"> Preguntas frecuentes </a> </li>
-                <li> <a href="#"> Noticias </a> </li>
-                <li> <a href="#"> Contacto </a> </li>
-            </ul>
-        </div>
+        <?php
+            wp_nav_menu(
+                array(
+                    'theme_location'  => 'primary',
+                    'container_class' => 'contenedor-menu',
+                    'fallback_cb'     => false,
+                )
+            );
+		?>
 
         <div class="contenedor-logo">
             <img src="<?php echo get_template_directory_uri().'/custom/img/logo-secretariadesalud.png';?>" alt="">
@@ -38,19 +34,15 @@
     <!-- Mobile -->
     <div class="contenedor-movil">
 
-        <div class="contenedor-menu">
-            <ul>
-                <li class="active"> <a href="#"> Home </a> </li>
-                <li> <a href="#"> Información de la vacuna </a> </li>
-                <li> <a href="#"> Priorización de personas a vacunar </a> </li>
-                <li> <a href="#"> Calendario vacunación </a> </li>
-                <li> <a href="#"> Eventos adversos </a> </li>
-                <li> <a href="#"> Datos </a> </li>
-                <li> <a href="#"> Preguntas frecuentes </a> </li>
-                <li> <a href="#"> Noticias </a> </li>
-                <li> <a href="#"> Contacto </a> </li>
-            </ul>
-        </div>
+        <?php
+            wp_nav_menu(
+                array(
+                    'theme_location'  => 'primary',
+                    'container_class' => 'contenedor-menu',
+                    'fallback_cb'     => false,
+                )
+            );
+		?>
 
     </div>
     <!-- Mobile -->
